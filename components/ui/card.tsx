1a2524922d97ef65d484a5d858c5d28d9d50dcca
Irfan Maulana <dev.irfanmaulana007@gmail.com>
2025-10-15 10:01:33 +0700

    about section refactor

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      'rounded-lg border bg-card text-card-foreground shadow-sm',
+      'rounded-lg border border-[#E5E5E5] bg-[#FAFAFA] dark:border-[#242424] dark:bg-[#1E1E1E] text-card-foreground shadow-sm',
       className
     )}
     {...props}
